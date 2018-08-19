e6db189e3bee98addb005ec81e6cfb8008158f98
Ivankivu <ivankivu@gmail.com>
2018-08-20 02:13:15 +0300

    Api update

--- api/v1/questions/put_api.py
+++ api/v1/questions/put_api.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, jsonify
 from flask_restful import Api, Resource, reqparse
 
@@ -45,34 +44,6 @@ api.add_resource(Questions, "/")
 
 class Question(Resource):
 
-        def get(self, method=["GET"]):
-            parser = reqparse.RequestParser()
-            parser.add_argument("question_title")
-            args = parser.parse_args()
-            for question in questions:
-                if(args["question_title"] == question["question_title"]):
-                    return question, 200
-            return "User not found", 404
-
-        def post(self, method=["POST"]):
-            parser = reqparse.RequestParser()
-            parser.add_argument("question_title")
-            parser.add_argument("question_id")
-            parser.add_argument("description")
-            args = parser.parse_args()
-
-            for question in questions:
-                if(args["question_title"] == question["question_title"]):
-                    return "Question with Title {} already exists".format(args["question_title"]), 400
-
-            question = {
-                "question_title": args["question_title"],
-                "question_id": args["question_id"],
-                "description": args["description"]
-            }
-            questions.append(question)
-            return question, 201
-
         def put(self, method=["PUT"]):
             parser = reqparse.RequestParser()
             parser.add_argument("question_title")
@@ -93,17 +64,8 @@ class Question(Resource):
                 "question_id": args["question_id"],
                 "occupation": args["occupation"]
             }
-            questions.append(question)
+            question["answer"].append(question)
             return question, 201
-
-        def delete(self, method=["DELETE"]):
-            parser = reqparse.RequestParser()
-            parser.add_argument("question_title")
-            args = parser.parse_args()
-            global questions
-            questions = [
-                question for question in questions if question["question_title"] != args["question_title"]]
-            return "{} is deleted.".format(args["question_title"]), 200
 
 
 api.add_resource(Question, "/")
