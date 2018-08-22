a165f466e24cff823412f54be85acb5627b45518
Ivankivu <ivankivu@gmail.com>
2018-08-22 10:11:13 +0300

    calibrating tests

--- api/v1/questions/test_app.py
+++ api/v1/questions/test_app.py
@@ -3,25 +3,30 @@
 import unittest
 import app 
 from app import questions
-from flask import Flask
+from flask import Flask, request
 import json
 import sys
+from flask import Request, Response
+from pipenv.vendor import urllib3
 
 class TestAPI(unittest.TestCase):
-        
+    
     def test_home(self):
-        response = self.app.get("/")
-        self.assertEquals(response.code, 200)
-        
+         self.BaseUrl = 'http://localhost:5000'
   
     def test_posts(self):
-
-        self.assertDictEqual("question", True)
+        PostUrl = (self.BaseUrl+"?q=question,questions"+"&"+"APPID=")
+        print (PostUrl)
+        response = urllib3.urlopen(PostUrl)
+        html=response.read()
+        print(html)
+        self.assertTrue("question" in html)
     
     def test_post_question(self):
         pass
     
     def test_put_question(self):
+        #self.assertDictEqual("/questions", True)
         pass
     
     def test_delete_question(self):
