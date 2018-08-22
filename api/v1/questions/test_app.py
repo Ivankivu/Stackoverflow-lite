c46779bc865ff9b5dc4dd2493ba85b2cb041c487
Ivankivu <ivankivu@gmail.com>
2018-08-22 06:08:12 +0300

    getting there!!
    api updates

---
+++ api/v1/questions/test_app.py
@@ -0,0 +1,28 @@
+
+import unittest
+import app 
+from flask import Flask
+import json
+import sys
+
+class TestAPI(unittest.TestCase):
+        
+#     def test_home(self):
+#         response = self.app.get("/")
+#         self.assertEquals(response.code, 200)
+        
+  
+    def test_posts(self):
+       pass
+    
+    def test_post_question(self):
+        pass
+    
+    def test_put_question(self):
+        pass
+    
+    def test_delete_question(self):
+        pass
+    
+    
+    
